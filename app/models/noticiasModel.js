d1abe66fe2aa5f390af073278163a6ec13e84afc
Sanzio Wesley <sanziowesley@hotmail.com>
2020-12-29 22:20:34 -0300

    inserting record in mysql

--- app/models/noticiasModel.js
+++ app/models/noticiasModel.js
@@ -8,5 +8,9 @@ module.exports = function(){
     connection.query('select * from noticias where id_noticia = 2', callback);
   }
 
+  this.salvarNoticia = function(noticia, connection, callback){
+    connection.query('insert into noticias set ? ', noticia, callback)
+  }
+
   return this;
 }
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,10 +1,19 @@
-module.exports = function(app){
-  app.get('/formulario_inclusao_noticia', function(req, res){
+module.exports = function(application){
+  application.get('/formulario_inclusao_noticia', function(req, res){
     res.render("admin/form_add_noticia.ejs"); 
   });
 
-  app.post('/noticias/salvar', function(req, res){
-    var noticias = req.body;
-    res.send(noticias); 
+  application.post('/noticias/salvar', function(req, res){
+    var noticia = req.body;
+
+
+    var connection = application.config.dbConnection();
+    var noticiasModel = application.app.models.noticiasModel;
+
+    noticiasModel.salvarNoticia(noticia, connection, function(error, result){
+      res.redirect('/noticias')
+    });
+
+
   });
 };
