0fe4c5d9c2c6a6237306e09c031b0a046ef65a1f
Sanzio Wesley <sanziowesley@hotmail.com>
2021-01-15 16:07:23 -0300

    including static resources

--- config/server.js
+++ config/server.js
@@ -4,10 +4,10 @@ var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 
 var app = express();
-
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
+app.use(express.static('./app/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
 
