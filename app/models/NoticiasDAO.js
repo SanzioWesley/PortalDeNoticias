2c9a40e5afdcb0d6d195b6f3162739f37dc89381
Sanzio Wesley <sanziowesley@hotmail.com>
2021-01-15 17:17:47 -0300

    reapplying the backend logic - part 1

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -14,6 +14,10 @@ NoticiasDAO.prototype.salvarNoticia = function (noticia, callback) {
     this._connection.query('INSERT INTO noticias SET ?', noticia, callback);
 }
 
+NoticiasDAO.prototype.get5UltimasNoticias = function (callback) {
+    this._connection.query('select * from noticias order by data_criacao desc limit 5', callback)
+}
+
 module.exports = function () {
     return NoticiasDAO;
 }
